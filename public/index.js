009fe3982ab705b3c9f468aa358f536c20fe5fb6
Cole Challand <ctchalland@gmail.com>
2018-03-06 09:01:33 -0600

    Changed the search place holder

--- public/index.js
+++ public/index.js
@@ -4,14 +4,14 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      // welcome: "Welcome to your Places",
-      // places: [
-      //   { id: 1, name: "Welcome to place 1", address: false },
-      //   { id: 2, name: "Welcome to taks 2", address: false },
-      //   { id: 3, name: "Welcome to task 3", address: false }
-      // ],
-      // place: { id: null, name: "", address: "" },
-      // chngePlace: { id: null, name: "", address: "" },
+      welcome: "Welcome to your Places",
+      places: [
+        { id: 1, name: "Welcome to place 1", address: false },
+        { id: 2, name: "Welcome to taks 2", address: false },
+        { id: 3, name: "Welcome to task 3", address: false }
+      ],
+      place: { id: null, name: "", address: "" },
+      chngePlace: { id: null, name: "", address: "" },
       mapPlaces: [
         {
           title: "Mark's House",
@@ -57,7 +57,9 @@ var HomePage = {
   },
   watch: {
     searchPlace: function() {
+      console.log(this);
       var convertSearchPlace = this.searchPlace.split(" ").join("+");
+
       axios
         .get(
           `https://maps.googleapis.com/maps/api/geocode/json?address=${convertSearchPlace}`
@@ -102,12 +104,11 @@ var HomePage = {
     }
   },
   mounted: function() {
-    // Not required for google maps
-    // axios.get("/places").then(
-    //   function(response) {
-    //     this.places = response.data;
-    //   }.bind(this)
-    // );
+    axios.get("/places").then(
+      function(response) {
+        this.places = response.data;
+      }.bind(this)
+    );
 
     var map = new google.maps.Map(document.getElementById("map"), {
       center: this.mapPlace.center,
@@ -142,39 +143,39 @@ var HomePage = {
     });
   },
   methods: {
-    // addPlace: function() {
-    //   var newTask = {
-    //     id: this.places.length + 1,
-    //     name: this.place.name,
-    //     address: this.place.address
-    //   };
-    //   axios
-    //     .post("/places", {
-    //       name: newTask.name,
-    //       address: false
-    //     })
-    //     .then(function(response) {
-    //       console.log(response);
-    //     });
-    //   this.places.push(newTask);
-    //   this.place.name = "";
-    // },
-    // changePlace: function() {
-    //   var changedPlace = {
-    //     id: this.chngePlace.id,
-    //     name: this.chngePlace.name
-    //   };
-    //   axios
-    //     .patch(`/places/${changedPlace.id}`, {
-    //       name: changedPlace.name
-    //     })
-    //     .then(function(response) {
-    //       console.log(response);
-    //     });
-    // },
-    // deleteAllTasks: function() {
-    //   this.places.splice(0, this.places.length);
-    // }
+    addPlace: function() {
+      var newTask = {
+        id: this.places.length + 1,
+        name: this.place.name,
+        address: this.place.address
+      };
+      axios
+        .post("/places", {
+          name: newTask.name,
+          address: false
+        })
+        .then(function(response) {
+          console.log(response);
+        });
+      this.places.push(newTask);
+      this.place.name = "";
+    },
+    changePlace: function() {
+      var changedPlace = {
+        id: this.chngePlace.id,
+        name: this.chngePlace.name
+      };
+      axios
+        .patch(`/places/${changedPlace.id}`, {
+          name: changedPlace.name
+        })
+        .then(function(response) {
+          console.log(response);
+        });
+    },
+    deleteAllTasks: function() {
+      this.places.splice(0, this.places.length);
+    }
   },
   computed: {}
 };
